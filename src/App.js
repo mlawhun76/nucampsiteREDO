0a222326394923aa7ddd540097053450c858f479
Coding Laptop <mlawhun76@gmail.com>
2024-02-03 23:41:06 -0500

    End of Workshop 1 Task 1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import './App.css';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
+import CampsiteCard from './features/campsites/CampsiteCard';
+import {CAMPSITES} from './app/shared/CAMPSITES';
 
 
 function App() {
@@ -14,7 +16,8 @@ function App() {
           </NavbarBrand>
         </Container>
       </Navbar>
-      I'm ready for workshop!
+
+      <CampsiteCard campsite={CAMPSITES[0]}/>
     </div>
   );
 }
