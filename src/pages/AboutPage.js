5149a3cc8466fa9723016490587f1be471d82ed1
Coding Laptop <mlawhun76@gmail.com>
2024-02-19 23:56:30 -0500

    Week2 Workshop Complete. Added list of partners on AboutPage

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,6 @@
 import {Col, Row, Container, Card, CardBody, CardHeader} from 'reactstrap';
 import SubHeader from '../components/SubHeader';
+import PartnersList from '../features/partners/PartnersList';
 
 const AboutPage = () => {
     return (
@@ -69,7 +70,7 @@ const AboutPage = () => {
                 </Col>
 
             <Row className='row-content'>
-
+                <PartnersList/>
             </Row>
         </Container>
     );
