5e3fe4f8c80218702981ec063e4ae4063f670b8f
Coding Laptop <mlawhun76@gmail.com>
2024-02-19 19:16:06 -0500

    Week 2 UseParams in DetialPage updated to render a page with the clicked campsite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {Routes, Route} from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
-import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import './App.css';
+
 
 
 function App() {
@@ -15,6 +17,9 @@ function App() {
           <Route path='/' element={<HomePage/>} />
           <Route path='contact' element={<ContactPage/>} />
           <Route path='directory' element={<CampsitesDirectoryPage/>} />
+          <Route path='directory/:campsiteId'
+                 element={<CampsiteDetailPage/>}
+          />
         </Routes>
       <Footer/>
     </div>
--- src/features/campsites/CampsiteCard.js
+++ src/features/campsites/CampsiteCard.js
@@ -1,23 +1,25 @@
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
+import {Link} from 'react-router-dom';
 
 //receives 'campsite' from CampsitesList.js. So... 'props' is actually 'campsite' passed in
 //Change 'props' to 'campsite' that's passed in
 const CampsiteCard = ({campsite}) => {
-    const {image, name} = campsite;
+    const {id, image, name} = campsite;
     return (
-        <Card>
-            <CardImg
-                width='100%'
-                src={image}
-                alt={name}
-            />
-
-            <CardImgOverlay>
-                <CardTitle>
-                    {name}
-                </CardTitle>
-            </CardImgOverlay>
-        </Card>
+        <Link to={`${id}`}>
+            <Card>
+                <CardImg
+                    width='100%'
+                    src={image}
+                    alt={name}
+                />
+                <CardImgOverlay>
+                    <CardTitle>
+                        {name}
+                    </CardTitle>
+                </CardImgOverlay>
+            </Card>
+        </Link>
     );
 };
 
