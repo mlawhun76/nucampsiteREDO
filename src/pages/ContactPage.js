a4fe658bc813d8ae5a9eec4305f07f8d3d321e94
Coding Laptop <mlawhun76@gmail.com>
2024-02-27 20:31:54 -0500

    Week 3 Form Validation Complete

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import { Container, Col, Row } from "reactstrap";
 import SubHeader from "../components/SubHeader";
+import ContactForm from "../components/ContactForm";
 
 
 const ContactPage = () => {
@@ -47,7 +48,7 @@ const ContactPage = () => {
                 </Col>
 
                 <Col md='10'>
-                    TBD: ContactForm
+                    <ContactForm />
 
                 </Col>
 
